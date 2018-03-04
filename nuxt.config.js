fa2ff3e0109525f03ed22b2f72de2a163b0cdf35
Александр Голубов <Alex@alex.local>
2018-03-04 13:10:40 +0500

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  base: '/vue_02/',
+} : {}
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'text_editor',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Text Editor' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the CSS
+  */
+  css: [
+    'tachyons/css/tachyons.min.css',
+    '~assets/index.css',
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  /*
+  ** Make router relative to /vue_portfolio_01
+  */
+  router: {
+    ...routerBase,
+  }
+}
